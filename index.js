be2c4c971b6e85b03f763d9c015dd07ee066c378
opama2 <95030993+opama2@users.noreply.github.com>
2023-11-27 21:50:50 +0300

    added functionality

--- index.js
+++ index.js
@@ -1,20 +1,163 @@
+// js dom
 let buttons = document.querySelectorAll("button");
-let operans = document.querySelectorAll("button.operands");
+let operands = document.querySelectorAll("button.operands");
 let operators = document.querySelectorAll("button.operator");
 let del = document.querySelector("button#delete");
 let allclear = document.querySelector("button#alclear");
+let display = document.querySelector(".display h1");
+let calc = document.querySelector(".calculate");
 
-let display = document.querySelector(".display");
 
-let result = ""
+// operator and operands
+
+let operand1 = ""
+let operrand2 = ""
+let operator = "" 
+let result; 
+
 
 
 buttons.forEach((x) => {
     x.addEventListener('click', (e) => {
-        result += x.innerHTML
-        display.innerHTML = result
+        if(x.className == "operator" && operator != "") {
+            operand1 = operate()
+            operrand2 = ""
+            operator = ""
+        }
+        if(x.className == "operator" && operator === "") {
+            operator += x.innerHTML
+        } 
 
+
+        if(x.className == "dot" && operator == "" && !operand1.includes(".") ) {
+            operand1 += x.innerHTML
+        }
+        if(x.className == "dot" && operator != "" && !operand1.includes(".") ) {
+            operrand2 += x.innerHTML
+        }
+
+
+
+        if(x.className == "operands" && operator == "") {
+            operand1 += x.innerHTML
+        } 
+        if(x.className == "operands" && operator != "" ) {
+            operrand2+= x.innerHTML
+        }
+    
+        
     })
 })
 
+
+function updisplay () {
+    result = `${operand1}${operator}${operrand2}`
+     display.innerHTML = result
+}
+
+buttons.forEach((x) =>  {
+    x.addEventListener('click', updisplay)
+})
+
+
+
+
+
+function add (a,b) {
+    return a+b;
+}
+function multiply (a,b) {
+    return a*b;
+}
+
+function substruct (a,b) {
+    return a-b;
+}
+function divide (a,b) {
+    if(b = 0) {
+        return "why you tryna   break my calc by dividing by zero"
+    }
+    return a/b;
+}
+
+function power (a,b) {
+    return a**b;
+}
+
+
+
+
+function allclearf () {
+ operand1 = ""
+ operrand2 = ""
+ operator = "" 
+ result; 
+}
+//
+
+
+function operate() { 
+    let solution;
+    switch(operator) {
+        case"+":
+        solution = add(Number(operand1),Number(operrand2));
+        break;
+
+        case"*":
+        solution = multiply(Number(operand1),Number(operrand2));
+        break;
+
+        case"-":
+        solution = substruct(Number(operand1),Number(operrand2));
+        break;
+
+        case"/":
+        solution = divide(Number(operand1),Number(operrand2));
+        if (solution === Infinity) {
+            solution = "why you tryna break my calc by dividing by zero";
+        }
+        break;
+
+        case"^":
+        solution = power(Number(operand1),Number(operrand2));
+        break;
+
+
+        default:
+        console.log("Invalid operator");
+        break;
+};
+
+display.innerHTML = solution;
+return solution;
+    
+
+}
+
+
+calc.addEventListener("click", operate)
+function clecal () {
+    operand1 = operate()
+    operrand2 = ""
+    operator = "" 
+   }
+calc.addEventListener("click", clecal)
+
+
+
+
+
+function backspace() {
+    if (operrand2 !== "") {
+        operrand2 = operrand2.slice(0, -1); // Remove the last character
+    } else if (operator !== "") {
+        operator = "";
+    } else if (operand1 !== "") {
+        operand1 = operand1.slice(0, -1); // Remove the last character
+    }
+
+    
+}
+
+del.addEventListener('click', backspace);
 
